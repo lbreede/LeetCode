762e0ee5c4c32aabd9a3324ae092c6f992b436cf
lbreede <67719520+lbreede@users.noreply.github.com>
2024-01-16 10:01:24 -0800

    :recycle: switched function name to camelCase to mirror LeetCode

--- python/1_two_sum.py
+++ python/1_two_sum.py
@@ -2,7 +2,7 @@ from itertools import permutations
 from typing import Optional
 
 
-def two_sum(nums: list[int], target: int) -> Optional[list[int]]:
+def twoSum(nums: list[int], target: int) -> Optional[list[int]]:
     for i, j in permutations(range(len(nums)), 2):
         if nums[i] + nums[j] == target:
             return [i, j]
@@ -10,15 +10,15 @@ def two_sum(nums: list[int], target: int) -> Optional[list[int]]:
 
 
 def main():
-    output = two_sum([2, 7, 11, 15], 9)
+    output = twoSum([2, 7, 11, 15], 9)
     assert output == [0, 1], f"Example 1 - Fail: {output}"
     print(f"Example 1 - Pass: {output}")
 
-    output = two_sum([3, 2, 4], 6)
+    output = twoSum([3, 2, 4], 6)
     assert output == [1, 2], f"Example 2 - Fail: {output}"
     print(f"Example 2 - Pass: {output}")
 
-    output = two_sum([3, 3], 6)
+    output = twoSum([3, 3], 6)
     assert output == [0, 1], f"Example 3 - Fail: {output}"
     print(f"Example 3 - Pass: {output}")
 
