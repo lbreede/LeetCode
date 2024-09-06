7fa69ede3a3033bca4310cfa60e1d45578a2bd2c
lbreede <67719520+lbreede@users.noreply.github.com>
2024-09-05 17:08:52 -0700

    black

--- Python3/02587_max_score.py
+++ Python3/02587_max_score.py
@@ -1,16 +1,18 @@
 from itertools import accumulate
+
+
 def maxScore(nums: list[int]) -> int:
-    return sum(x>0 for x in accumulate(sorted(nums, reverse=True)))
+    return sum(x > 0 for x in accumulate(sorted(nums, reverse=True)))
+
 
 def main() -> None:
-    output = maxScore([2,-1,0,1,-3,3,-3])
+    output = maxScore([2, -1, 0, 1, -3, 3, -3])
     assert output == 6, f"Example 1 - Fail: {output}"
     print(f"Example 1 - Pass: {output}")
 
-    output = maxScore([-2,-3,0])
+    output = maxScore([-2, -3, 0])
     assert output == 0, f"Example 2 - Fail: {output}"
     print(f"Example 2 - Pass: {output}")
-    
 
 
 if __name__ == "__main__":
